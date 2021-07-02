4891ce495cb3ee4431e1bc3d3dc7e2b7be46d1a6
Dalitso kaputa <48954069+kaputadali@users.noreply.github.com>
2021-07-02 15:43:45 +0100

    code updated with the problem being solved

--- index.js
+++ index.js
@@ -2,19 +2,45 @@ const Logger = require('./Logger')
 const fs = require('fs')
 const path = require('path')
 
-
+// instantiate a new logger
 const logger = new Logger()
 
 logger.on('message', data => {
     console.log('Called Listener:', data)
-    fs.appendFile(path.join(__dirname, '/logger', 'logger.txt'), `\n ${data}`, err=>{
-        if(err) throw err
-        console.log('file written...')
-    })
+
+//    fs.appendFile(path.join(__dirname, '/logger', 'logger.txt'), `\n ${data}`, err => {
+//         if (err) throw err
+//         console.log('file written...')
+//     })
+fs.appendFile(path.join(__dirname, '/logger', 'logger.txt'), `\n ${JSON.stringify(data)}`, err => {
+    if (err) throw err
+    console.log('file written...')
+})
+   
 })
 
 
 logger.log('Hello there')
 
+// const Logger = require('./Logger')
+// const fs = require('fs')
+// const path = require('path')
 
+// // instantiate a new logger 
+
+// const logger = new Logger()
+
+// // Create write stream
+// const wstream = fs.createWriteStream(
+//   path.join(__dirname, '/logger', 'logger.txt'), 
+//   { 
+//     flags: 'a', // append to file
+//     autoClose: false // leave fd open for more writing after buffer empties
+//   }
+// );
+
+// logger.on('message', data => {
+//   console.log('Called Listener:', data)
+//   wstream.write(`${data.uuid} - ${data.log}\n`); 
+// })
 
